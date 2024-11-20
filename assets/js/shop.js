a9c6707cf7473c1dff3c27d5c7a16419ecfed6e2
Javid Assia <jial17@gmail.com>
2024-11-20 14:31:30 -0500

    agregando paginación y más ´roductos a bd_productos

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /* Funciones para pintar cart de compras*/
 const cards = document.getElementById("cards");
+const paginationContainer = document.getElementById("pagination");
+const rowsPerPage = 6;
+let currentPage = 1;
 const items = document.getElementById("items");
 const templateCard = document.getElementById("template-cards").content;
 const templateSize = document.getElementById("size").content;
@@ -27,31 +30,40 @@ let cart = {};
 async function fetchData() {
   try {
     const resp = await fetch(
-      "https://raw.githubusercontent.com/Jiassia17/proyecto-ttc-ecomerce/refs/heads/main/bd_json/productos.json"
+      "./bd_json/productos.json"
+  /*     "https://raw.githubusercontent.com/Jiassia17/proyecto-ttc-ecomerce/refs/heads/main/bd_json/productos.json" */
     );
     const data = await resp.json();
-    paintCards(data); /* Llamar a la función */
+    //console.log(data);
+    paintCards(data, cards, rowsPerPage, currentPage); /* Llamar a la función */
+    setupPagination(data, paginationContainer, rowsPerPage);
   } catch (error) {
     console.log(error);
   }
 }
 
-function paintCards(data) {
+function paintCards(data, wrapper, rowsPerPage, page) {
   //console.log(data);
-  data.forEach((producto) => {
-    templateCard.querySelector("h6").textContent = producto.nombre;
-    templateCard.querySelector("#precio").textContent = producto.precio;
-    templateCard.querySelector("img").setAttribute("src", producto.imagen);
-    templateCard.querySelector(".btn-buy").dataset.id = producto.id;
-    templateCard
-      .querySelector(".size")
-      .appendChild(crearItems(producto.tallas));
+  wrapper.innerHTML = "";
+  page--;
+
+  let start = rowsPerPage * page;
+  let end = start + rowsPerPage;
+  let paginatedItems = data.slice(start, end);
+  //console.log(paginatedItems);
+  for (let i = 0; i < paginatedItems.length; i++) {
+    let item = paginatedItems[i];
+    templateCard.querySelector("h6").textContent = item.nombre;
+    templateCard.querySelector("#precio").textContent = item.precio;
+    templateCard.querySelector("img").setAttribute("src", item.imagen);
+    templateCard.querySelector(".btn-buy").dataset.id = item.id;
+    templateCard.querySelector(".size").appendChild(crearItems(item.tallas));
     const clone = templateCard.cloneNode(true);
     fragment.appendChild(clone);
     templateCard
       .querySelector(".size")
       .removeChild(templateCard.querySelector(".items"));
-  });
+  }
   cards.appendChild(fragment);
 }
 
@@ -81,7 +93,7 @@ function setCart(objeto) {
     id: objeto.querySelector(".btn-buy").dataset.id,
     nombre: objeto.querySelector("h6").textContent,
     precio: objeto.querySelector("#precio").textContent,
-    imagen: objeto.querySelector("img").getAttribute('src'),
+    imagen: objeto.querySelector("img").getAttribute("src"),
     cantidad: 1,
   };
   if (cart.hasOwnProperty(producto.id)) {
@@ -101,3 +113,34 @@ cards.addEventListener("click", (e) => {
 items.addEventListener("click", (e) => {
   btnAccion(e);
 });
+
+/* Paginación */
+function setupPagination(items, wrapper, rowsPerPage) {
+  wrapper.innerHTML = "";
+
+  let pageCount = Math.ceil(items.length / rowsPerPage);
+  for (let i = 1; i < pageCount + 1; i++) {
+    let btn = paginationButton(i, items);
+    wrapper.appendChild(btn);
+  }
+}
+
+function paginationButton(page, items) {
+  let button = document.createElement("button");
+  button.classList.add("btn", "btn-outline-dark");
+  button.innerText = page;
+
+  if (currentPage == page) button.classList.add("active");
+
+  button.addEventListener("click", function () {
+    //console.log("Hiciste click en el botón de la página", page);
+    currentPage = page;
+    
+    paintCards(items, cards, rowsPerPage, currentPage);
+    let currentBtn = document.querySelector(".pagination button.active");
+    currentBtn.classList.remove("active");
+    button.classList.add("active");
+  });
+
+  return button;
+}
